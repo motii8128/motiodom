7874e2709d2573781dae803e95a17ab09c04e342
motii8128 <default270robo@gmail.com>
2024-04-14 22:47:02 +0900

    Update

--- include/matrix_utils.hpp
+++ include/matrix_utils.hpp
@@ -3,9 +3,30 @@
 
 namespace imu_localizer_cpp
 {
-    class Vector3
+    template<typename T>
+    struct Vector3
     {
+        T x, y, z;
 
+        Vector3(T x_, T y_, T z_):x(x_),y(y_),z(z_){}
+    };
+
+    template<typename T>
+    class Matrix3x3
+    {
+        public:
+        Matrix3x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_, T m31_, T m32_, T m33_):
+        m11(m11_),m12(m12_),m13(m13_),
+        m21(m21_),m22(m22_),m23(m23_),
+        m31(m31_),m32(m32_),m33(m33_){}
+
+        Matrix3x3 transpose()
+        {
+            
+        }
+
+        private:
+        T m11, m12, m13, m21, m22, m23, m31, m32, m33;
     };
 }
 
