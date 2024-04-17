0644ae2adb5ce388b8641983d10879821f510b16
motii8128 <default270robo@gmail.com>
2024-04-17 10:01:28 +0900

    Update ekf_utils

--- include/ekf_utils.hpp
+++ include/ekf_utils.hpp
@@ -8,8 +8,26 @@ namespace motiodom
     template<typename T>
     class AccelAngularEKF
     {
-        Vector3<T> estimation;
-        Matrix3x3<T> cov;
+        public:
+        AccelAngularEKF(float delta_time);
+
+
+        private:
+        Matrix2x3<T> h();
+        Matrix3x3<T> calc_jacob(Vector3<T> input_matrix);
+        void predict_x(Vector3<T> input_matrix);
+        void predict_cov(Matrix3x3<T> jacob);
+        Vector2<T> update_residual(Vector2<T> observation);
+        Matrix2x2<T> update_s();
+        Matrix3x2<T> update_kalman_gain(Matrix2x2<T> s);
+        void update_x(Vector2<T> residual);
+        void update_cov();
+
+        Vector3<T> estimation_;
+        Matrix3x3<T> cov_;
+        Matrix3x3<T> estimation_noise_;
+        Matrix2x2<T> observation_noise_;
+        Matrix3x2<T> kalman_gain_;
     };
 }
 
--- include/matrix_utils.hpp
+++ include/matrix_utils.hpp
@@ -12,6 +12,15 @@ namespace motiodom
     };
 
     template<typename T>
+    struct Vector2
+    {
+        T x, y;
+
+        Vector2(T x_, T y_):x(x_), y(y_){}
+    };
+    
+
+    template<typename T>
     class Matrix3x3
     {
         public:
@@ -58,12 +67,38 @@ namespace motiodom
         Matrix3x2(T m11_, T m12_, T m21_, T m22_, T m31_, T m32_):
         m11(m11_),m12(m12_),
         m21(m21_),m22(m22_),
-        m31(m31_),m32(m32_),
+        m31(m31_),m32(m32_){}
+
+        Matrix2x3<T> transpose()
         {
-            
+            return Matrix2x3<T>(
+                m11, m21, m31,
+                m12, m22, m32,
+            );
         }
         private:
         T m11, m12, m21, m22, m31, m32;
+    };
+
+    template<typename T>
+    class Matrix2x3
+    {
+        public:
+        Matrix2x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_):
+        m11(m11_),m12(m12_),m13(m13_),
+        m21(m21_),m22(m22_),m23(m23_){}
+
+        Matrix3x2<T> transpose()
+        {
+            return Matrix3x2<T>(
+                m11, m21, 
+                m12, m22,
+                m13, m23
+            );
+        }
+
+        private:
+        T m11, m12, m13, m21, m22, m23;
     };
 }
 
