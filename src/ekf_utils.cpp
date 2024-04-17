977053209788ec7046e774698ec79df2a385189c
motii8128 <default270robo@gmail.com>
2024-04-17 10:59:47 +0900

    Update ekf_utlis.cpp

--- src/ekf_utils.cpp
+++ src/ekf_utils.cpp
@@ -4,8 +4,30 @@
 namespace motiodom
 {
     template<typename T>
-    AccelAngularEKF::AccelAngularEKF(float delta_time)
+    AccelAngularEKF<T>::AccelAngularEKF(float delta_time)
     {
+        estimation_ = Vector3<T>(0.0, 0.0, 0.0);
+        estimation_noise_ = Matrix3x3<T>(
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0
+        );
 
+        cov_ = Matrix3x3<T>(
+            0.0174*delta_time*delta_time, 0.0, 0.0,
+            0.0, 0.0174*delta_time*delta_time, 0.0,
+            0.0, 0.0, 0.0174*delta_time*delta_time
+        );
+
+        observation_noise_ = Matrix2x2<T>(
+            0.0, 0.0,
+            0.0, 0.0
+        );
+
+        kalman_gain_ = Matrix3x2<T>(
+            0.0, 0.0,
+            0.0, 0.0,
+            0.0, 0.0
+        );
     }
 }
