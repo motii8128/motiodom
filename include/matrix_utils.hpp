4202ff39721bff5b4fe81208d4b37cf2c92883fe
motii8128 <default270robo@gmail.com>
2024-04-17 21:47:51 +0900

    Update matrix utils

--- include/matrix_utils.hpp
+++ include/matrix_utils.hpp
@@ -8,7 +8,7 @@ namespace motiodom
     {
         T x, y, z;
 
-        Vector3(T x_, T y_, T z_):x(x_),y(y_),z(z_){}
+        Vector3(T x_, T y_, T z_);
     };
 
     template<typename T>
@@ -16,78 +16,50 @@ namespace motiodom
     {
         T x, y;
 
-        Vector2(T x_, T y_):x(x_), y(y_){}
+        Vector2(T x_, T y_);
     };
     
 
     template<typename T>
-    class Matrix3x3
+    struct Matrix3x3
     {
-        public:
         Matrix3x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_, T m31_, T m32_, T m33_);
 
-        Matrix3x3<T> transpose();
-
-        private:
         T m11, m12, m13, m21, m22, m23, m31, m32, m33;
     };
 
     template<typename T>
-    class Matrix2x2
+    struct Matrix2x2
     {
-        public:
         Matrix2x2(T m11_, T m12_, T m21_, T m22_);
 
-        Matrix2x2<T> transpose()
-        {
-            return Matrix2x2<T>(
-                m11, m21, 
-                m12, m22);
-        }
-
-        private:
         T m11, m12, m21, m22,
     };
 
     template<typename T>
-    class Matrix3x2
+    struct Matrix3x2
     {
-        Matrix3x2(T m11_, T m12_, T m21_, T m22_, T m31_, T m32_):
-        m11(m11_),m12(m12_),
-        m21(m21_),m22(m22_),
-        m31(m31_),m32(m32_){}
-
-        Matrix2x3<T> transpose()
-        {
-            return Matrix2x3<T>(
-                m11, m21, m31,
-                m12, m22, m32,
-            );
-        }
-        private:
+        Matrix3x2(T m11_, T m12_, T m21_, T m22_, T m31_, T m32_);
+        
         T m11, m12, m21, m22, m31, m32;
     };
 
     template<typename T>
-    class Matrix2x3
+    struct Matrix2x3
     {
-        public:
-        Matrix2x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_):
-        m11(m11_),m12(m12_),m13(m13_),
-        m21(m21_),m22(m22_),m23(m23_){}
+        Matrix2x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_);
 
-        Matrix3x2<T> transpose()
-        {
-            return Matrix3x2<T>(
-                m11, m21, 
-                m12, m22,
-                m13, m23
-            );
-        }
-
-        private:
         T m11, m12, m13, m21, m22, m23;
     };
+
+    template<typename T>
+    Matrix3x3<T> transpose_3x3(Matrix3x3<T> matrix);
+
+    template<typename T>
+    Matrix2x2<T> transpose_2x2(Matrix2x2<T> matrix);
+
+    template<typename T>
+    Matrix2x3<T> transpose_3x2(Matrix3x2<T> matrix);
 }
 
 #endif 
--- src/matrix_utils.cpp
+++ src/matrix_utils.cpp
@@ -3,20 +3,16 @@
 namespace motiodom
 {
     template<typename T>
+    Vector3<T>::Vector3(T x_, T y_, T z_):x(x_),y(y_),z(z_){}
+
+    template<typename T>
+    Vector2<T>::Vector2(T x_, T y_):x(x_),y(y_){}
+
+    template<typename T>
     Matrix3x3<T>::Matrix3x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_, T m31_, T m32_, T m33_):
     m11(m11_),m12(m12_),m13(m13_),
         m21(m21_),m22(m22_),m23(m23_),
         m31(m31_),m32(m32_),m33(m33_){}
-
-    template<typename T>
-    Matrix3x3<T> Matrix3x3<T>::transpose()
-    {
-        return Matrix3x3<T>(
-            m11, m21, m31,
-            m12, m22, m23,
-            m13, m23, m33,
-        );
-    }
 
     template<typename T>
     Matrix2x2<T>::Matrix2x2(T m11_, T m12_, T m21_, T m22_):
@@ -24,11 +20,34 @@ namespace motiodom
         m21(m21_), m22(m22_){}
 
     template<typename T>
-    Matrix2x2<T> Matrix2x2<T>::transpose()
+    Matrix3x2<T>::Matrix3x2(T m11_, T m12_, T m21_, T m22_, T m31_, T m32_):
+    m11(m11_),m12(m12_),
+    m21(m21_),m22(m22_),
+    m31(m31_),m32(m32_){}
+
+    template<typename T>
+    Matrix2x3<T>::Matrix2x3(T m11_, T m12_, T m13_, T m21_, T m22_, T m23_):
+    m11(m11_),m12(m12_),m13(m13_),
+    m21(m21_),m22(m22_),m23(m23_){}
+
+    template<typename T>
+    Matrix3x3<T> transpose_3x3(Matrix3x3<T> matrix)
     {
-        return Matrix2x2<T>(
-            m11, m21,
-            m12, m22
+        return Matrix3x3<T>(
+            matrix.m11, matrix.m21, matrix.m31,
+            matrix.m12, matrix.m22, matrix.m23,
+            matrix.m13, matrix.m23, matrix.m33,
         );
     }
+
+    template<typename T>
+    Matrix2x2<T> transpose_2x2(Matrix2x2<T> matrix)
+    {
+        return Matrix2x2<T>(
+            matrix.m11, matrix.m21,
+            matrix.m12, matrix.m22
+        );
+    }
+
+    
 }
